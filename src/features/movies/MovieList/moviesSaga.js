688e52e120bbc687b57a6f1a6a10bdfd093ee9c0
RobertOsial <rob.osial@gmail.com>
2022-08-04 07:21:34 +0100

    formatted code in moviesSaga file

--- src/features/movies/MovieList/moviesSaga.js
+++ src/features/movies/MovieList/moviesSaga.js
@@ -1,8 +1,12 @@
-import { debounce, delay, call, put, takeLatest, all } from "redux-saga/effects";
 import {
-  APIpopularMoviesUrl,
-  APIsearchMovieUrl,
-} from "../../APIdata";
+  debounce,
+  delay,
+  call,
+  put,
+  takeLatest,
+  all,
+} from "redux-saga/effects";
+import { APIpopularMoviesUrl, APIsearchMovieUrl } from "../../APIdata";
 import { getAPI } from "../../getAPI";
 import {
   fetchMovies,
@@ -17,15 +21,17 @@ function* fetchMoviesWorker({ payload: { query, page } }) {
   const urlPath = !query ? popularMovies : searchMovie;
 
   try {
-    if (!query) { yield delay(300) };
+    if (!query) {
+      yield delay(300);
+    }
     const requestMovies = yield call(getAPI, urlPath);
     yield put(fetchMoviesSuccess(requestMovies));
   } catch (error) {
     yield put(fetchMoviesError());
   }
-};
+}
 
 export function* watchFetchMovies() {
   yield debounce(2000, fetchMoviesSearch.type, fetchMoviesWorker);
   yield takeLatest(fetchMovies.type, fetchMoviesWorker);
-};
+}
